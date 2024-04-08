47d6c617742e2d34dbf1935436d755b5a246e73d
Nwaiwuchi10 <djnchrys@gmail.com>
2024-04-08 17:20:20 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./components/Homepage/HomePage";
+import AdminLayout from "./admin/AdminDashboard/AdminLayout";
+
+function App() {
+  return (
+    <div>
+      <AdminLayout />
+    </div>
+  );
+}
+
+export default App;
